c25c5016f67b6f535b2dae131e03d50225f5cdd5
jialanxin <jialanxin1996@hotmail.com>
2025-04-01 17:15:38 +0800

    chore: Update file list in main.py

--- main.py
+++ main.py
@@ -141,13 +141,13 @@ if __name__ == "__main__":
     # 文件路径设置
     new_file = "merged_eml_content.txt"  # 新文件路径
     old_files = [  # 旧文件路径列表
-        "merged_eml_content_20250227_222432.txt",
         "merged_eml_content_20250303_194342.txt",
 	    "merged_eml_content_20250312_164613.txt",
         "merged_eml_content_20250317_182409.txt",
         "merged_eml_content_20250319_162840.txt",
         "merged_eml_content_20250323_213542.txt",
         "merged_eml_content_20250326_210237.txt",
+        "merged_eml_content_20250401_165747.txt",
         # 可以添加更多旧文件...
     ]
     output_file = "FileScholar.md"  # 输出文件名
